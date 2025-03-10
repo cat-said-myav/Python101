7d542e8bf243e2e9d2ebfc618c5048979081463d
cat-said-myav <mrrrmyav@gmail.com>
2025-03-10 18:20:50 +0300

    Add files via upload

---
+++ Лабораторная работа 1/task_2.py
@@ -0,0 +1,10 @@
+
+disket_size = 1.44
+book_size = 100 * 50 * 25 * 4 / 1024 / 1024
+
+book_count = round(disket_size / book_size)
+
+# TODO Найдите количество книг, которое можно разместить на дискете
+
+print("Количество книг, помещающихся на дискету:", book_count)
+
